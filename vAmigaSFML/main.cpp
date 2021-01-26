b7ad8ad8962143895fa65ca0530be86ddcaaff7b
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-01-26 07:22:35 +0100

    Initial Commit

---
+++ vAmigaSFML/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  vAmigaSFML
+//
+//  Created by Dirk Hoffmann on 26.01.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
