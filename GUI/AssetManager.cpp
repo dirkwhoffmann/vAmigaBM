8b0bd42a2007949e1cf165fb688285350f8f14bc
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-03-17 19:47:42 +0100

    Work on icons

--- GUI/AssetManager.cpp
+++ GUI/AssetManager.cpp
@@ -19,24 +19,24 @@ TextureManager::load(TextureID id)
             
         case TextureID::logo: path = "logo.png"; break;
         case TextureID::title: path = "title.png"; break;
-        case TextureID::disk: path = "disk.png"; break;
-        case TextureID::diskProtected: path = "disk_protected.png"; break;
+        case TextureID::disk: path = "disk36.png"; break;
+        case TextureID::diskProtected: path = "disk_protected36.png"; break;
         case TextureID::diskLarge: path = "disk_large.png"; break;
-        case TextureID::spin: path = "spin.png"; break;
+        case TextureID::spin: path = "spin36.png"; break;
         case TextureID::ledBlack: path = "led_black.png"; break;
         case TextureID::ledGrey: path = "led_grey.png"; break;
         case TextureID::ledGreen: path = "led_green.png"; break;
         case TextureID::ledRed: path = "led_red.png"; break;
-        case TextureID::mute: path = "mute.png"; break;
-        case TextureID::halt: path = "halt.png"; break;
-        case TextureID::sync: path = "sync.png"; break;
-        case TextureID::warp: path = "warp.png"; break;
-        case TextureID::syncLock: path = "sync_lock.png"; break;
-        case TextureID::warpLock: path = "warp_lock.png"; break;
-        case TextureID::none: path = "none.png"; break;
+        case TextureID::mute: path = "mute36.png"; break;
+        case TextureID::halt: path = "halt36.png"; break;
+        case TextureID::sync: path = "sync36.png"; break;
+        case TextureID::warp: path = "warp36.png"; break;
+        case TextureID::syncLock: path = "sync_lock36.png"; break;
+        case TextureID::warpLock: path = "warp_lock36.png"; break;
+        case TextureID::none: path = "none36.png"; break;
         case TextureID::mouse: path = "mouse36.png"; break;
-        case TextureID::joystick: path = "joystick.png"; break;
-        case TextureID::keyset: path = "keyset.png"; break;
+        case TextureID::joystick: path = "joystick36.png"; break;
+        case TextureID::keyset: path = "keyset36.png"; break;
             
         default:
             assert(false);
